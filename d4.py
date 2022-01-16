9c0e32959264eba1e9d623087106139ca477263f
lykius <lucadeluigi91@gmail.com>
2022-01-16 14:37:30 +0100

    chore: change prints

--- d4.py
+++ d4.py
@@ -11,11 +11,11 @@ def main() -> None:
     dep_trainer = D4DepthTrainer()
     dep_trainer.train()
 
-    print("Training D4 semantics network...")
+    print("\nTraining D4 semantics network...")
     sem_trainer = D4SemanticsTrainer()
     sem_trainer.train()
 
-    print("Training D4 transfer network...")
+    print("\nTraining D4 transfer network...")
     transfer_trainer = D4TransferTrainer()
     transfer_trainer.train()
 
